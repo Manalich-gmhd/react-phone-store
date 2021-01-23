c58a6e6c24c5b9ca858dcfa26e921aa10dde8b36
Manalich-gmhd <rey_manalich@yahoo.es>
2021-01-22 19:49:30 -0500

    paypal button

--- src/components/Cart/CartTotals.js
+++ src/components/Cart/CartTotals.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
-export default function CartTotals({ value }) {
+import PayPalButton from './PayPalButton';
+export default function CartTotals({ value,history }) {
   const { cartSubtotal, cartTax, cartTotal, clearCart } = value;
   return (
     <React.Fragment>
@@ -37,6 +37,7 @@ export default function CartTotals({ value }) {
                 $ {cartTotal}
               </strong>
             </h5>
+            <PayPalButton total={cartTotal} clearCart={clearCart} history={history}/>
 
           </div>
         </div>
